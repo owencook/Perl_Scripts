7b9c58a4518bdef831300f8ab98cf5700a773783
Owen Cook <rcook@pcug.org.au>
2018-01-06 14:28:52 +1100

    Initial commit

---
+++ Read.vmsg.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+use utf8;
+use strict;
+
+my @matches;
+
+while (<DATA>) {
+
+    my $line = $_;
+    next unless ( $line =~ /=/ );
+    chomp $line;
+
+    @matches = $line =~ /=(\w+)/g;
+
+    foreach my $letter (@matches) {
+        next if ( $letter =~ /"QUOTED"/ );
+        next if ( $letter =~ /"UTF"/ );
+        printf( "%c", hex($letter) );
+    }
+}
+print "\n";
+
+__DATA__
+BEGIN:VMSG 
+VERSION:1.1 
+BEGIN:VCARD 
+TEL:+61490000000 
+END:VCARD 
+BEGIN:VBODY 
+X-BOX:INBOX 
+X-READ:READ 
+X-SIMID:0 
+X-LOCKED:UNLOCKED 
+X-TYPE:SMS 
+Date:2016/04/06 09:13:21 
+Subject;ENCODING=QUOTED-PRINTABLE;CHARSET=UTF-8:=4F=6B=2E=20=53=74=61= 
+=79=20=63=6F=6D=66=79=2E 
+END:VBODY 
+END:VMSG
---
+++ otp.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl -w
+
+# program to generate an alphabet table for use with a otp
+
+use strict;
+
+my @alpha = 'A' .. 'Z';
+
+my $first = $alpha[0];
+print "   @alpha\n\n";
+
+my $n = scalar(@alpha);
+
+foreach my $i ( 1 .. $n ) {
+
+    my $first = $alpha[0];
+    print "$first  @alpha\n";
+
+    push( @alpha, shift @alpha );
+}
+
