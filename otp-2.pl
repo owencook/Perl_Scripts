db43f6ad762f55d304eeacbc49e5a44f4ec3c95e
owencook <rcook@pcug.org.au>
2018-01-08 09:27:31 +1100

    Reduced the randomnumbers to 50 per line

--- otp-2.pl
+++ otp-2.pl
@@ -8,7 +8,7 @@ my $string;
 my $rng;
 open my $RNGS, ">", "rngs.txt" or die "Can't open rngs.txt $!\n";
 foreach my $l ( 0 .. 9 ) {  # Generate 10 lines of 55 random characters from the pool of '0 ..9' and 'a .. z'
-    $rng = join '', map +( 0 .. 9, 'a' .. 'z' )[ rand( 10 + 26 ) ], 1 .. 55;
+    $rng = join '', map +( 0 .. 9, 'a' .. 'z' )[ rand( 10 + 26 ) ], 1 .. 50;
     print $RNGS "$rng\n";
 
 }
@@ -16,11 +16,11 @@ close $RNGS;
 
 open my $RNGS, "<", "rngs.txt" or die "Can't open rngs.txt $!\n";
 
-while (<$RNGS>) { # now extract 10 blocks of 5 characters, initial offset is 5 so we lose 5 of the 55
+while (<$RNGS>) { # now extract 10 blocks of 5 characters
     chomp;
     my $string = $_;
 
-    foreach my $j ( 5, 10, 15, 20, 25, 30, 35, 40, 45, 50 ) {
+    foreach my $j (0, 5, 10, 15, 20, 25, 30, 35, 40, 45 ) {
         foreach my $l ( 0 .. 4 ) {
             my $char = substr( $string, $l + $j, 1 );
             print "$char";
