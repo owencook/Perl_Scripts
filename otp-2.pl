9f065c332ae31ecee459aa3e26913f88f861bd8e
owencook <rcook@pcug.org.au>
2018-01-07 16:36:51 +1100

    Initial commit

---
+++ otp-2.pl
@@ -0,0 +1,35 @@
+#!/usr/bin/perl
+
+my $count;
+my $string;
+
+# 
+
+my $rng;
+open my $RNGS, ">", "rngs.txt" or die "Can't open rngs.txt $!\n";
+foreach my $l ( 0 .. 9 ) {  # Generate 10 lines of 55 random characters from the pool of '0 ..9' and 'a .. z'
+    $rng = join '', map +( 0 .. 9, 'a' .. 'z' )[ rand( 10 + 26 ) ], 1 .. 55;
+    print $RNGS "$rng\n";
+
+}
+close $RNGS;
+
+open my $RNGS, "<", "rngs.txt" or die "Can't open rngs.txt $!\n";
+
+while (<$RNGS>) { # now extract 10 blocks of 5 characters, initial offset is 5 so we lose 5 of the 55
+    chomp;
+    my $string = $_;
+
+    foreach my $j ( 5, 10, 15, 20, 25, 30, 35, 40, 45, 50 ) {
+        foreach my $l ( 0 .. 4 ) {
+            my $char = substr( $string, $l + $j, 1 );
+            print "$char";
+        }
+        print " ";
+    }
+    print "\n";
+    $count++;
+
+    #print "$count\n";
+    if ( ( $count % 5 ) == 0 ) { print "\n"; } 
+}
